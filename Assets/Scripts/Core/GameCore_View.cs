361547d0f70b4d35225e15d4bd748167885d1b60
angus <anguskungcn@gmail.com>
2025-02-06 14:39:59 +0800

    opt: remove redundant code

--- Assets/Scripts/Core/GameCore_View.cs
+++ Assets/Scripts/Core/GameCore_View.cs
@@ -8,9 +8,6 @@ namespace Core
         {
             // todo: register all views here
             UI.RegisterUiData(new UIData(UIName.Menu, "Assets/GameResources/Prefab/UI/MenuView.prefab"));
-            // UI.RegisterUiData(new UIData(UIName.LevelHud, "Assets/GameResources/Prefab/UI/LevelHudView.prefab"));
-            // UI.RegisterUiData(new UIData(UIName.Plot, "Assets/GameResources/Prefab/UI/PlotView.prefab"));
-            // UI.RegisterUiData(new UIData(UIName.SpecialHud, "Assets/GameResources/Prefab/UI/SpecialHudView.prefab"));
         }
     }
 }
